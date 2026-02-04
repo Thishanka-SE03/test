fae135a38801200bb7383dab16b838c5f582f941
Thishanka-SE03 <thishanka.bsc.se03@edu.lnbti.lk>
2026-02-04 23:43:47 +0530

    feat: add centered login link to EmailConfirmed component

--- src/auth/Confirmed.jsx
+++ src/auth/Confirmed.jsx
@@ -68,6 +68,14 @@ const EmailConfirmed = () => {
               Please log back into the <b>EcoSphere app</b> to continue.
             </p>
           </div>
+
+          <a
+            href="/login"
+            className={styles.btnPrimary}
+            style={{ textAlign: "center" }}
+          >
+            Go to Login
+          </a>
         </div>
       </section>
     </main>
